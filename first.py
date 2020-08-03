43472d69eda7123409683df259735cb9533103c7
EJ-0624 <68366624+EJ-0624@users.noreply.github.com>
2020-08-03 22:55:04 +0800

    Update first.py

--- first.py
+++ first.py
@@ -4,6 +4,15 @@ Created on Tue Jul 14 17:34:39 2020
 
 @author: User
 """
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+mc.postToChat("I'm watching you")
+
+while True:
+    x, y, z = mc.player.getTilePos()
+    mc.postToChat("You are located on X:" + str(x) 
+                  + ", Y:" +str(y) + ", Z:" + str(z))
 
 from mcpi.minecraft import Minecraft
 import timemc = Minecraft.create()
@@ -33,12 +42,5 @@ mc.setBlock(x-1, y-1, z+1 ,206)
 mc.setBlock(x+1, y-1, z+1 ,206)
 mc.setBlock(x+1, y-1, z-1 ,206)
 mc.setBlock(x-1, y-1, z-1,206)
-
-mc.postToChat("I'm watching you")
-
-while True:
-    x, y, z = mc.player.getTilePos()
-    mc.postToChat("You are located on X:" + str(x) 
-                  + ", Y:" +str(y) + ", Z:" + str(z))
 
 
