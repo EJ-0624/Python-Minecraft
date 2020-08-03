55e0ae239f3073a58b8b1983e243615af5532b65
EJ-0624 <68366624+EJ-0624@users.noreply.github.com>
2020-08-03 20:49:13 +0800

    Update first.py

--- first.py
+++ first.py
@@ -6,6 +6,22 @@ Created on Tue Jul 14 17:34:39 2020
 """
 
 from mcpi.minecraft import Minecraft
+import timemc = Minecraft.create()
+time.sleep(5)
+x,y,z =mc.player.getTilePos()
+mc.setBlock(x,y,z,206)
+mc.setBlock(x,y+1,z,206)
+mc.setBlock(x,y+2,z,206)
+mc.setBlock(x,y+3,z,206)
+mc.setBlock(x,y+4,z,206)
+mc.setBlock(x,y+5,z,206)
+mc.setBlock(x,y+6,z,206)
+mc.setBlock(x,y+7,z,206)
+mc.setBlock(x,y+8,z,206)
+mc.setBlock(x,y+9,z,206)
+
+
+from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
 x, y, z = mc.player.getTilePos()
